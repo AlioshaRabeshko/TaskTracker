abcfbf49c7d2f91ffb0e5d961be9cd5614db0932
demetrisKPI <muzhnat@ukr.net>
2020-11-04 23:42:22 +0200

    minor-changes

--- front/src/Components/TaskList.js
+++ front/src/Components/TaskList.js
@@ -1,36 +1,32 @@
 const TaskList = () => {
   const tasks = [
     {
-      name:'Do anal',
-      desc: 'Must cum',
+      name:'Do work',
+      desc: 'Make .psd maket',
       deadline: new Date(),
       prior: 10,
-      time: 'Infinity',
+      time: '4 hours',
+      state: 'Pending',
+    },
+    {
+      name:'Do work',
+      desc: 'Make .psd maket',
+      deadline: new  Date(),
+      prior: 10,
+      time: '4 hours',
       state: 'In progress',
     },
     {
-      name:'Do anal',
-      desc: 'Must cum',
+      name:'Do work',
+      desc: 'Make .psd maket',
       deadline: new  Date(),
       prior: 10,
-      time: 'Infinidsafdty',
-      state: 'In progress',
-    },
-    {
-      name:'Do anal',
-      desc: 'Must cum',
-      deadline: new  Date(),
-      prior: 10,
-      time: 'Infinity',
-      state: 'In progress',
+      time: '4 hours',
+      state: 'Done',
     },
   ];
   return (
     <div className="Container">
-        <p>
-          This is how much you've already done.<br></br>
-          Not really much, though. In fact, you fucking did nothing, go work, prick
-        </p>
         <ul className="list">
           <li className="list-title">
             <h3>Name</h3>
@@ -38,7 +34,8 @@ const TaskList = () => {
             <h3>Deadline</h3>
             <h3>Priority</h3>
             <h3>Time</h3>
-            <h3 className="last">State</h3>
+            <h3>State</h3>
+            <h3>Done</h3>
           </li>
           {tasks.map((el, id) => 
             <li key={id}>
@@ -48,6 +45,7 @@ const TaskList = () => {
               <p>{el.prior}/10</p>
               <p>{el.time}</p>
               <p>{el.state}</p>
+              <p><input type="checkbox"/></p>
             </li>)}
         </ul>
     </div>
