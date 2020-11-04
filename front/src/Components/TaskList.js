b8aead46f2248a576e5ca129f14e48c859807dd1
AlioshaRabeshko <LEflylll@gmail.com>
2020-11-04 22:19:19 +0200

    list-style

--- front/src/Components/TaskList.js
+++ front/src/Components/TaskList.js
@@ -1,53 +1,56 @@
-const TaskList = () =>
-  <div className="Container">
-      <p>
-        This is how much you've already done.<br></br>
-        Not really much, though. In fact, you fucking did nothing, go work, prick
-      </p>
-      <table className="Tasklist">
-        <thead>
-        <tr>
-          <th>Task name</th>
-          <th>Description</th>
-          <th>Deadline</th>
-          <th>Priority</th>
-          <th>Time</th>
-          <th>State</th>
-          <th>Mark as done</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <td>Do anal</td>
-          <td>Must cum</td>
-          <td>15.15.2015 15:15</td>
-          <td>10/10</td>
-          <td>Infinity</td>
-          <td>In progress</td>
-          <td>
-            <input type="checkbox"></input>
-          </td>
-        </tr>
-        <tr>
-          <td>Do anal</td>
-          <td>Must cum</td>
-          <td>15.15.2015 15:15</td>
-          <td>10/10</td>
-          <td>Infinity</td>
-          <td>In progress</td>
-          <td><input type="checkbox"></input></td>
-        </tr>
-        <tr>
-          <td>Do anal</td>
-          <td>Must cum</td>
-          <td>15.15.2015 15:15</td>
-          <td>10/10</td>
-          <td>Infinity</td>
-          <td>In progress</td>
-          <td><input type="checkbox"></input></td>
-        </tr>
-        </tbody>
-      </table>
-  </div>;
+const TaskList = () => {
+  const tasks = [
+    {
+      name:'Do anal',
+      desc: 'Must cum',
+      deadline: new Date(),
+      prior: 10,
+      time: 'Infinity',
+      state: 'In progress',
+    },
+    {
+      name:'Do anal',
+      desc: 'Must cum',
+      deadline: new  Date(),
+      prior: 10,
+      time: 'Infinidsafdty',
+      state: 'In progress',
+    },
+    {
+      name:'Do anal',
+      desc: 'Must cum',
+      deadline: new  Date(),
+      prior: 10,
+      time: 'Infinity',
+      state: 'In progress',
+    },
+  ];
+  return (
+    <div className="Container">
+        <p>
+          This is how much you've already done.<br></br>
+          Not really much, though. In fact, you fucking did nothing, go work, prick
+        </p>
+        <ul className="list">
+          <li className="list-title">
+            <h3>Name</h3>
+            <h3>Description</h3>
+            <h3>Deadline</h3>
+            <h3>Priority</h3>
+            <h3>Time</h3>
+            <h3 className="last">State</h3>
+          </li>
+          {tasks.map((el, id) => 
+            <li key={id}>
+              <p>{el.name}</p>
+              <p>{el.desc}</p>
+              <p>{el.deadline.toDateString()}</p>
+              <p>{el.prior}/10</p>
+              <p>{el.time}</p>
+              <p>{el.state}</p>
+            </li>)}
+        </ul>
+    </div>
+)};
 
 export default TaskList;
