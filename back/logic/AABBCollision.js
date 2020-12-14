3bdb991086276a25f35eeb9b3bbe6e109a43761c
YaroslavChirko <31710480+YaroslavChirko@users.noreply.github.com>
2020-12-14 17:35:37 +0200

    fixed, added, almost tested

--- back/logic/AABBCollision.js
+++ back/logic/AABBCollision.js
@@ -1,167 +1,225 @@
-const { Sequelize } = require('sequelize');
-const TimeFormatter = require('./timeFormatting');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 const Task = require('./models/task_db');
+const TF = require('./time_formatting').TimeFormatter;
 
-let tasksLeft = null;
-let tasksRight = null;
-
-function getCollidingNum(uid, deadline, wholeTime) {
+async function getCollidingNum(uid, deadline, whole_time) {
   // change to async
-  Task.findAll({
+  const tasks = await Task.findAll({
     where: {
       state: 'pending',
       uid: uid,
-      raw: true,
-      bestStartTime: {
-        [Sequelize.Op.gte]: deadline - wholeTime,
-        deadline: { [Sequelize.Op.lte]: deadline },
+      best_start_time: {
+        [Sequelize.Op.gte]: deadline - whole_time,
+        [Sequelize.Op.lte]: deadline,
+      },
+      deadline: {
+        [Sequelize.Op.lte]: deadline,
+        [Sequelize.Op.gte]: deadline - whole_time,
       },
     },
-  })
-    .then((tasks) => tasks.length)
-    .catch((err) => console.error(err));
+  });
+
+  return tasks.length;
 }
 
-function getAllTasks(uid, deadline) {
-  // change to async method
-  Task.findAll({
+async function getCollidingNum(uid, deadline, whole_time, task_id) {
+  // change to async
+  const tasks = await Task.findAll({
     where: {
       state: 'pending',
       uid: uid,
-      raw: true,
+      best_start_time: {
+        [Sequelize.Op.gte]: deadline - whole_time,
+        [Sequelize.Op.lte]: deadline,
+      },
+      deadline: {
+        [Sequelize.Op.lte]: deadline,
+        [Sequelize.Op.gte]: deadline - whole_time,
+      },
+      id: {
+        [Sequelize.Op.ne]: task_id,
+      },
+    },
+  });
+
+  return tasks.length;
+}
+
+async function getAllTasks(uid, deadline) {
+  // change to async method
+
+  const tasks1 = await Task.findAll({
+    where: {
+      state: 'pending',
+      uid: uid,
       deadline: { [Sequelize.Op.lte]: deadline },
     },
-    order: [['deadline', 'INC']],
-  })
-    .then((tasks) => {
-      tasksLeft = tasks;
-    })
-    .catch((err) => console.error(err));
+    order: [['deadline', 'ASC']],
+  });
 
-  Task.findAll({
+  const tasks2 = await Task.findAll({
     where: {
       state: 'pending',
       uid: uid,
-      raw: true,
       deadline: { [Sequelize.Op.gt]: deadline },
     },
-    order: [['deadline', 'INC']],
-  })
-    .then((tasks) => {
-      tasksRight = tasks;
-    })
-    .catch((err) => console.error(err));
+    order: [['deadline', 'ASC']],
+  });
+  return [tasks1, tasks2];
 }
 
-export default function checkTime(
+async function getAllTasks(uid, deadline, task_id) {
+  const tasks1 = await Task.findAll({
+    where: {
+      state: 'pending',
+      uid: uid,
+      deadline: { [Sequelize.Op.lte]: deadline },
+      id: { [Sequelize.Op.ne]: task_id },
+    },
+    order: [['deadline', 'ASC']],
+  });
+
+  const tasks2 = await Task.findAll({
+    where: {
+      state: 'pending',
+      uid: uid,
+      deadline: { [Sequelize.Op.gt]: deadline },
+      id: { [Sequelize.Op.ne]: task_id },
+    },
+    order: [['deadline', 'ASC']],
+  });
+  return [tasks1, tasks2];
+}
+
+module.exports.check_time = async function check_time(
   uid,
   deadline,
-  initialTime,
-  additionalTime,
-  priority
+  initial_time,
+  additional_time,
+  execution_time = '00:00:00',
+  priority,
+  task_id = null
 ) {
-  let wholeTime =
-    TimeFormatter.toNormalTime(initialTime) +
-    TimeFormatter.toNormalTime(additionalTime);
-  getCollidingNum(uid, deadline, wholeTime).then((collNum) => {
-    if (collNum === 0) {
-      return deadline - wholeTime;
+  let whole_time =
+    TF.toNormalTime(initial_time) + TF.toNormalTime(additional_time);
+  if (execution_time != '00:00:00') {
+    // had to put it in here due to update possibility
+    whole_time -= TF.toNormalTime(execution_time);
+  }
+  let collNum;
+  if (task_id === null) {
+    collNum = await getCollidingNum(uid, deadline, whole_time);
+  } else {
+    collNum = await getCollidingNum(uid, deadline, whole_time, task_id);
+  }
+  if (collNum === 0) {
+    return deadline - whole_time;
+  } else {
+    if (task_id === null) {
+      [tasks_left, tasks_right] = await getAllTasks(uid, deadline);
     } else {
-      getAllTasks(uid, deadline).then(() => {
-        let availableTime = deadline - Date.now(); //divide by number of working hours
-        let leftTime = 0;
-        tasksLeft.forEach((task) => {
-          leftTime +=
-            TimeFormatter.toNormalTime(task.initialTime) +
-            TimeFormatter.toNormalTime(task.additionalTime) -
-            TimeFormatter.toNormalTime(task.executionTime);
-        });
-        const realDeadline = deadline - tasksRight[0].bestStartTime;
-
-        if (realDeadline < 0) {
-          realDeadline = 0;
-        }
-
-        if (availableTime - leftTime > wholeTime + realDeadline) {
-          const { lastLeftDeadline, leftArr } = reduceShiftScopeLeft(
-            tasksLeft,
-            wholeTime,
-            availableTime
-          );
-          return leftShift(leftArr, lastLeftDeadline);
-        } else if (
-          0 < availableTime - leftTime &&
-          availableTime - leftTime < wholeTime + realDeadline
-        ) {
-          const { overdueTime, leftArr } = findLowPriorityLeft(
-            tasksLeft,
-            wholeTime,
-            availableTime,
-            priority
-          );
-          const bstCurrent = leftShift(leftArr, null);
-          if (overdueTime > 0 && realDeadline > 0) {
-            //check for ability of right shift
-            const { overdueTime, rightArr, changed } = checkForRightShift(
-              tasksRight,
-              overdueTime,
-              priority
-            );
-            if (changed === 1) {
-              Task.update(
-                {
-                  bestStartTime: rightArr[0].bestStartTime,
-                  additionalTime: rightArr[0].additionalTime,
-                },
-                {
-                  where: {
-                    uid: rightArr[0].uid,
-                  },
-                }
-              ).catch((err) => console.error(err));
-            } else if (changed > 1) {
-              overdueTime = rightShift(
-                rightArr,
-                overdueTime,
-                changed,
-                priority,
-                deadline,
-                realDeadline
-              );
-            }
-          }
-          //print delay message
-          console.dir('expected overdue: ' + overdueTime);
-          return bstCurrent; // change to normal work hours
-        } else {
-          // add as is, print error message
-          console.dir('not enough time');
-          return deadline - wholeTime; // change to normal work hours
-        }
-      });
+      [tasks_left, tasks_right] = await getAllTasks(uid, deadline, task_id);
     }
-  });
-}
+    let available_time = deadline - Date.now(); //divide by number of working hours
+    let left_time = 0;
+    for (task of tasks_left) {
+      left_time +=
+        TF.toNormalTime(task.initial_time) +
+        TF.toNormalTime(task.additional_time) -
+        TF.toNormalTime(task.execution_time);
+    }
 
-function leftShift(leftArr, lastLeftDeadline) {
+    let real_deadline = 0;
+    if (tasks_right.length != 0) {
+      real_deadline = deadline - tasks_right[0].best_start_time;
+    }
+    if (real_deadline < 0) {
+      real_deadline = 0;
+    }
+    if (available_time - left_time > whole_time + real_deadline) {
+      /* let lastLeftDeadline, left_arr;
+      [lastLeftDeadline, left_arr] = reduceShiftScopeLeft(
+        tasks_left,
+        whole_time,
+        available_time
+      );
+      console.dir(lastLeftDeadline);*/
+      return leftShift(tasks_left, null /*left_arr, lastLeftDeadline*/);
+    } else if (
+      0 < available_time - left_time &&
+      available_time - left_time < whole_time + real_deadline
+    ) {
+      let overdue_time, left_arr;
+      [overdue_time, left_arr] = findLowPriorityLeft(
+        tasks_left,
+        whole_time,
+        available_time,
+        priority
+      );
+
+      let bst_current = leftShift(left_arr, null);
+      if (overdue_time > 0 && real_deadline > 0) {
+        //check for ability of right shift
+        let changed = 0;
+        let right_arr = null;
+        [overdue_time, right_arr, changed] = checkForRightShift(
+          tasks_right,
+          overdue_time,
+          priority
+        );
+        if (changed === 1) {
+          Task.update(
+            {
+              best_start_time: right_arr[0].best_start_time,
+              additional_time: right_arr[0].additional_time,
+            },
+            {
+              where: {
+                uid: right_arr[0].uid,
+              },
+            }
+          ).catch((err) => console.error(err));
+        } else if (changed > 1) {
+          overdue_time = rightShift(
+            right_arr,
+            overdue_time,
+            changed,
+            priority,
+            deadline,
+            real_deadline
+          );
+        }
+      }
+      //print delay message
+      console.dir('expected overdue: ' + overdue_time);
+      return bst_current; // change to normal work hours
+    } else {
+      // add as is, print error message
+      console.dir('not enough time');
+      return deadline - whole_time; // change to normal work hours
+    }
+  }
+};
+
+function leftShift(left_arr, lastLeftDeadline) {
   // null if we shift all
-  leftArr.forEach((task) => {
+  for (task of left_arr) {
     if (lastLeftDeadline === null) {
-      task.bestStartTime = Date.now();
-    } else if (task.bestStartTime > lastLeftDeadline) {
-      task.bestStartTime = lastLeftDeadline;
+      task.best_start_time = Date.now();
+    } else if (task.best_start_time > lastLeftDeadline) {
+      task.best_start_time = lastLeftDeadline;
     }
 
     lastLeftDeadline =
-      task.bestStartTime +
-      TimeFormatter.toNormalTime(task.initialTime) +
-      TimeFormatter.toNormalTime(task.additionalTime) -
-      TimeFormatter.toNormalTime(task.executionTime); // change to normal work hours
+      Date.parse(task.best_start_time) +
+      TF.toNormalTime(task.initial_time) +
+      TF.toNormalTime(task.additional_time) -
+      TF.toNormalTime(task.execution_time); // change to normal work hours
     //push changes, mb add method to
     Task.update(
       {
-        bestStartTime: task.bestStartTime,
-        additionalTime: task.additionalTime,
+        best_start_time: task.best_start_time,
+        additional_time: task.additional_time,
       },
       {
         where: {
@@ -169,172 +227,171 @@ function leftShift(leftArr, lastLeftDeadline) {
         },
       }
     ).catch((err) => console.error(err));
-  });
+  }
   return lastLeftDeadline;
 }
 
-function reduceShiftScopeLeft(leftArr, wholeTime, availableTime) {
-  availableTime += Date.now();
+function reduceShiftScopeLeft(left_arr, whole_time, available_time) {
+  available_time += Date.now();
   lastLeftDeadline = null;
-  leftArr.forEach((task) => {
-    availableTime -=
-      this.bestStartTime +
-      TimeFormatter.toNormalTime(task.initialTime) +
-      TimeFormatter.toNormalTime(task.additionalTime) -
-      TimeFormatter.toNormalTime(task.executionTime); // change to normal work hours
-    return wholeTime >= availableTime
-      ? leftArr.shift()
-      : { lastLeftDeadline, leftArr };
-  });
+  for (task of left_arr) {
+    lastLeftDeadline =
+      task.best_start_time +
+      TF.toNormalTime(task.initial_time) +
+      TF.toNormalTime(task.additional_time) -
+      TF.toNormalTime(task.execution_time);
+    available_time -= lastLeftDeadline; // change to normal work hours
+    if (whole_time >= available_time) {
+      left_arr.shift();
+    } else {
+      return [lastLeftDeadline, left_arr];
+    }
+  }
+  return [lastLeftDeadline, left_arr];
 }
 
 function findLowPriorityLeft(
-  leftArr,
-  wholeTime,
-  availableTime,
-  currentPriority
+  left_arr,
+  whole_time,
+  available_time,
+  current_priority
 ) {
-  let overdueTime = wholeTime - availableTime;
-  leftArr.forEach((task) => {
-    if (currentPriority > task.priority) {
-      const additionalDifference = TimeFormatter.toNormalTime(
-        task.additionalTime / 10
+  let overdue_time = whole_time - available_time;
+  for (task of left_arr) {
+    if (current_priority > task.priority) {
+      let additional_difference = TF.toNormalTime(task.additional_time / 10);
+      task.additional_time = TF.toTimeString(
+        TF.toNormalTime(task.additional_time) - additional_difference
       );
-      task.additionalTime = TimeFormatter.toTimeString(
-        TimeFormatter.toNormalTime(task.additionalTime) - additionalDifference
-      );
-      overdueTime -= additionalDifference;
-      if (overdueTime <= 0) {
+      overdue_time -= additional_difference;
+      if (overdue_time <= 0) {
         break;
       }
     }
-  });
-  return { overdueTime, leftArr };
+  }
+  return [overdue_time, left_arr];
 }
 
 function checkForRightShift(
-  rightArr,
-  overdueTime,
-  currentPriority,
+  right_arr,
+  overdue_time,
+  current_priority,
   deadline,
-  realDeadline
+  real_deadline
 ) {
   //check if can be shifted or if any additional time can be reduced
   if (
-    rightArr[0].deadline -
-      TimeFormatter.toNormalTime(rightArr[0].initialTime) -
-      TimeFormatter.toNormalTime(rightArr[0].additionalTime) >
-    rightArr[0].bestStartTime
+    right_arr[0].deadline -
+      TF.toNormalTime(right_arr[0].initial_time) -
+      TF.toNormalTime(right_arr[0].additional_time) >
+    right_arr[0].best_start_time
   ) {
     //should iterate to determine if shift is possible, also reduce additional time if needed
     //first  iterate to find unshiftable(one with deadline - initial -additional == bst)
-    let toChange = 0;
-    rightArr.forEach((task) => {
+    let to_change = 0;
+    for (task of right_arr) {
       if (
         task.deadline -
-          TimeFormatter.toNormalTime(task.initialTime) -
-          TimeFormatter.toNormalTime(task.additionalTime) <=
-        task.bestStartTime
+          TF.toNormalTime(task.initial_time) -
+          TF.toNormalTime(task.additional_time) <=
+        task.best_start_time
       ) {
-        if (task.priority < currentPriority) {
-          task.additionalTime = TimeFormatter.toTimeString(
-            TimeFormatter.toNormalTime(task.additionalTime) -
-              TimeFormatter.toNormalTime(task.additionalTime / 10)
+        if (task.priority < current_priority) {
+          task.additional_time = TF.toTimeString(
+            TF.toNormalTime(task.additional_time) -
+              TF.toNormalTime(task.additional_time / 10)
           );
-          toChange++;
+          to_change++;
         }
         break;
       } else {
-        toChange++;
+        to_change++;
       }
-    });
+    }
 
-    return { overdueTime, rightArr, changed: toChange };
+    return [overdue_time, right_arr, to_change];
   } else {
     //cannot shift, can only check first for additional time reduce
-    if (rightArr[0].priority < currentPriority) {
-      const additionalDifference = TimeFormatter.toNormalTime(
-        rightArr[0].additionalTime / 10
+    if (right_arr[0].priority < current_priority) {
+      let additional_difference = TF.toNormalTime(
+        right_arr[0].additional_time / 10
       );
-      rightArr[0].additionalTime = TimeFormatter.toTimeString(
-        TimeFormatter.toNormalTime(rightArr[0].additionalTime) -
-          additionalDifference
+      right_arr[0].additional_time = TF.toTimeString(
+        TF.toNormalTime(right_arr[0].additional_time) - additional_difference
       );
-      rightArr[0].bestStartTime -= additionalDifference;
-      overdueTime -= Math.max(
+      right_arr[0].best_start_time -= additional_difference;
+      overdue_time -= Math.max(
         Math.min(
-          realDeadline - rightArr[0].bestStartTime,
-          deadline - rightArr[0].bestStartTime
+          real_deadline - right_arr[0].best_start_time,
+          deadline - right_arr[0].best_start_time
         ),
         0
       );
-      return { overdueTime, rightArr, changed: 1 };
+      return { overdue_time, right_arr, changed: 1 };
     } else {
-      return { overdueTime, rightArr, changed: 0 };
+      return { overdue_time, right_arr, changed: 0 };
     }
   }
 }
 
 function rightShift(
-  rightArr,
-  overdueTime,
+  right_arr,
+  overdue_time,
   changed,
-  currentPriority,
+  current_priority,
   deadline,
-  realDeadline
+  real_deadline
 ) {
   //idk how to use priority here properly,should shift, change priority and shift again mb
-  let lastRightBst = Number.MAXSAFEINTEGER;
+  let lastRightBst = Number.MAX_SAFE_INTEGER;
   for (let i = changed - 1; i >= 0; i--) {
-    rightArr[i].bestStartTime = Math.min(
-      rightArr[i].deadline -
-        TimeFormatter.toNormalTime(rightArr[i].initialTime) -
-        TimeFormatter.toNormalTime(rightArr[i].additionalTime),
+    right_arr[i].best_start_time = Math.min(
+      right_arr[i].deadline -
+        TF.toNormalTime(right_arr[i].initial_time) -
+        TF.toNormalTime(right_arr[i].additional_time),
       lastRightBst
     );
-    lastRightBst = rightArr[i].bestStartTime;
+    lastRightBst = right_arr[i].best_start_time;
   }
-  if (deadline > rightArr[0].bestStartTime) {
-    rightArr.forEach((task) => {
-      if (task.priority < currentPriority) {
-        let additionalDifference = TimeFormatter.toNormalTime(
-          task.additionalTime / 10
+  if (deadline > right_arr[0].best_start_time) {
+    for (task of right_arr) {
+      if (task.priority < current_priority) {
+        let additional_difference = TF.toNormalTime(task.additional_time / 10);
+        task.additional_time = TF.toTimeString(
+          TF.toNormalTime(task.additional_time) - additional_difference
         );
-        task.additionalTime = TimeFormatter.toTimeString(
-          TimeFormatter.toNormalTime(task.additionalTime) - additionalDifference
-        );
-        task.bestStartTime -= additionalDifference;
+        task.best_start_time -= additional_difference;
       }
-    });
+    }
   }
-  lastRightBst = Number.MAXSAFEINTEGER;
+  lastRightBst = Number.MAX_SAFE_INTEGER;
   for (let i = changed - 1; i >= 0; i--) {
-    rightArr[i].bestStartTime = Math.min(
-      rightArr[i].deadline -
-        TimeFormatter.toNormalTime(rightArr[i].initialTime) -
-        TimeFormatter.toNormalTime(rightArr[i].additionalTime),
+    right_arr[i].best_start_time = Math.min(
+      right_arr[i].deadline -
+        TF.toNormalTime(right_arr[i].initial_time) -
+        TF.toNormalTime(right_arr[i].additional_time),
       lastRightBst
     );
-    lastRightBst = rightArr[i].bestStartTime;
+    lastRightBst = right_arr[i].best_start_time;
     Task.update(
       {
-        bestStartTime: rightArr[i].bestStartTime,
-        additionalTime: rightArr[i].additionalTime,
+        best_start_time: right_arr[i].best_start_time,
+        additional_time: right_arr[i].additional_time,
       },
       {
         where: {
-          uid: rightArr[i].uid,
+          uid: right_arr[i].uid,
         },
       }
     ).catch((err) => console.error(err));
   }
 
-  overdueTime -= Math.max(
+  overdue_time -= Math.max(
     Math.min(
-      realDeadline - rightArr[0].bestStartTime,
-      deadline - rightArr[0].bestStartTime
+      real_deadline - right_arr[0].best_start_time,
+      deadline - right_arr[0].best_start_time
     ),
     0
   );
-  return overdueTime;
+  return overdue_time;
 }
